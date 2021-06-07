1717e1781081798710d691586e091c76caeaa9ac
Peter Bruckner <peterbruckner08@gmail.com>
2021-06-07 21:11:34 +0930

    finished init files

---
+++ seeds/games-seeds.js
@@ -0,0 +1,18 @@
+
+
+const gameData = [
+    {
+        home_team_id: ,
+        away_team_id: ,
+        date: ,
+        time: ,
+        location: ,
+    },
+    {
+        home_team_id: ,
+        away_team_id: ,
+        date: ,
+        time: ,
+        location: ,
+    },
+]

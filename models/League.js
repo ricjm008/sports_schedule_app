1bd718374646b19ab9782f7b2e38fa3b5db04b46
Jared <ricjm008@mymail.unisa.edu.au>
2021-06-08 18:43:27 +0930

    created league model

---
+++ models/League.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcryptjs');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    validPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+}
+
+// Remember id, created_date, updated_date columns are created for you.
+User.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        // Hooks are automatic methods that run during various phases of the User Model lifecycle
+        // In this case, before a User is created/updated, we will automatically hash their password
+        hooks: {},
+        sequelize,
+        modelName: 'league',
+    }
+);
+
+module.exports = User;

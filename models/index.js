f066a54c1d5406464488b7fbdb39fb248407c57d
Jared <ricjm008@mymail.unisa.edu.au>
2021-06-08 19:43:59 +0930

    added new models to index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,12 @@
 const User = require('./user');
-
+const Game = require('./Game');
+const League = require('./League');
+const Schedule = require('./Schedule');
+const Team = require('./Team');
 module.exports = {
     User,
+    Game,
+    League,
+    Schedule,
+    Team
 };

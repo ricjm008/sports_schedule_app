c46f6c3040b30b0fcd7c68afc800c05c365860fb
Jared <ricjm008@mymail.unisa.edu.au>
2021-06-10 21:44:42 +0930

    created seed data

--- seeds/teams-seeds.js
+++ seeds/teams-seeds.js
@@ -1,6 +1,40 @@
-
-const teamData = [
+const leagueData = [
     {
-        
+        name: "Division 1",
+        season: "2021"
     }
 ]
+const teamData = [
+    {
+        name: "Crazy Frogs",
+        location: "Funky Town Stadium",
+        logo: 0,
+        leagueId: 1
+    },
+    {
+        name: "New York Knicks",
+        location: "Madison Square Gardens",
+        logo: 0,
+        leagueId: 1
+    },
+    {
+        name: "Justice League",
+        location: "Metropolis",
+        logo: 0,
+        leagueId: 1
+    },
+    {
+        name: "Red Hot Chilli Peppers",
+        location: "Flavortown",
+        logo: 0,
+        leagueId: 1
+    },
+    {
+        name: "Tasmanian Tigers",
+        location: "Australian National Museum",
+        logo: 0,
+        leagueId: 1
+    },
+]
+
+module.exports = { leagueData, teamData };

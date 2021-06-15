4ab6ac173376f7095e28499c7dfdf349a85bee7b
Jared <ricjm008@mymail.unisa.edu.au>
2021-06-15 19:04:36 +0930

    removed schedule table

--- models/index.js
+++ models/index.js
@@ -2,24 +2,21 @@ const User = require('./user');
 const League = require('./League');
 const Team = require('./Team');
 const Game = require('./Game');
-const Schedule = require('./Schedule');
 const UserFollowing = require('./UserFollowing');
 const TeamRecord = require('./TeamRecord');
 
 User.belongsToMany(Team, { through: UserFollowing, foreignKey: 'userId' });
 Team.belongsToMany(User, { through: UserFollowing, foreignKey: 'teamId' });
-Team.belongsToMany(Game, { through: TeamRecord, foreignKey: 'teamId' });
+Team.belongsToMany(Game, { through: TeamRecord, foreignKey: 'h_team_id' });
+Team.belongsToMany(Game, { through: TeamRecord, foreignKey: 'a_team_id' });
 Game.belongsToMany(Team, { through: TeamRecord,  foreignKey: 'gameId' });
 Team.belongsTo(League);
 League.hasMany(Team);
-Game.belongsTo(Schedule);
-Schedule.hasMany(Game);
 
 module.exports = {
     User,
     Game,
     League,
-    Schedule,
     Team,
     UserFollowing,
     TeamRecord
